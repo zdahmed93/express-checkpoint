f271f4c777d272d5a1e3e4ac77bf5d51cfb00ebd
zdahmed93 <ahmedzribiahmed@gmail.com>
2020-09-19 15:01:55 +0100

    add a middleware to check the current time

--- server.js
+++ server.js
@@ -2,36 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  //res.send("hi")
+app.use((re, res, next) => {
   const currentDate = new Date();
-  console.log(currentDate);
   const currentHour = currentDate.getHours();
-  console.log(currentHour);
-
   if (currentHour < 17 && currentHour >= 8) {
-    res.sendFile(__dirname + "/home.html");
+    next()
   } else res.send("<h1>Site is open from 08.00 to 17.00</h1>");
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
 });
 
 app.get("/services", (req, res) => {
-  const currentDate = new Date();
-  const currentHour = currentDate.getHours();
-
-  if (currentHour < 17 && currentHour >= 8) {
-    res.sendFile(__dirname + "/ourservices.html");
-  } else res.send("<h1>Site is open from 08.00 to 17.00</h1>");
+  res.sendFile(__dirname + "/ourservices.html");
 });
 
 app.get("/contact", (req, res) => {
-  const currentDate = new Date();
-  const currentHour = currentDate.getHours();
-
-  if (currentHour < 17 && currentHour >= 8) {
     res.sendFile(__dirname + "/contact.html");
-  } else res.send("<h1>Site is open from 08.00 to 17.00</h1>");
-
-  res.sendFile(__dirname + "/contact.html");
 });
 
 app.listen(3000, () => {
